7e9d8ebcf85b2556a4d23fc200dbe8b252715833
Chris.Chen <Chris.Chen@splashtop.com>
2025-01-16 19:07:45 +0800

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 # build.sh
 #!/bin/bash
 # 安裝 Python 依賴
+
+PROJECT_ROOT=$(pwd)
+
+echo "===========Installing Python dependencies============"
+cd $PROJECT_ROOT/backend
 pip install -r requirements.txt
 
 # 安裝前端依賴並編譯
+echo "===========Building frontend for production============"
+cd $PROJECT_ROOT/frontend
 cd frontend
 npm install
 npm run build
 
-# 回到後端目錄收集靜態文件
-cd ../backend
+echo "===========Building backend for production============"
+cd $PROJECT_ROOT/backend
 python manage.py collectstatic --noinput
 python manage.py migrate  # 執行資料庫遷移
+
+echo "================Project has been built successfully===================="
