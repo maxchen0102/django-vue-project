4cc1de3621696a61a9562151f8502ca65d70d7fd
Chris.Chen <Chris.Chen@splashtop.com>
2025-01-17 17:42:19 +0800

    ADD PWA test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+      registerType: 'prompt',
+      manifest: {
+        name: '您的應用程式名稱',
+        short_name: '短名稱',
+        description: '應用程式描述',
+        theme_color: '#41b883',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+
   server: {
     proxy: {
       '/api': {
@@ -12,6 +40,7 @@ export default defineConfig({
       }
     }
   },
+
   build: {
     outDir: '../backend/static/dist',  // 指向 Django 的靜態文件目錄
     assetsDir: 'assets', // css image 檔案放置目錄, 也在dist 底下
@@ -24,4 +53,6 @@ export default defineConfig({
       }
     }
   },
+
+
 })
